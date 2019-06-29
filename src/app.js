b28be9ff48d96d460916dcd07cbcb7879eba1b05
Aman <rumman.djuice1@gmail.com>
2019-06-30 01:40:51 +0200

    React-Hot-Reload added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 import React, { Component} from "react";
+import { hot } from 'react-hot-loader/root';
 import "./App.css";
 
+import Child from './components/child'
+
 class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {name:''};
+    }
     render(){
         return(
             <div className="App">
-                <h1> Hello, React-tive Aman</h1>
+                <h1> Hello, React-tive Aman...</h1>
+                <input type="text" value={this.state.name} onChange={(event)=>this.setState({name:event.target.value})}/>
+                <p>{this.state.name}lala</p>
+                <Child/>
             </div>
         );
     }
 }
 
-export default App;
+export default hot(App);
+//export default App;
---
+++ src/components/child.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Child = () => {
+    return (
+        <div>
+            <h2>I am a Child Component...</h2>
+
+        </div>
+    );
+};
+
+export default Child;
